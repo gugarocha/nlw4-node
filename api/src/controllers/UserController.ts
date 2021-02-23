f683d700ce03e6db4cd7bbcf6801f9b7be711b89
Gustavo <guga.rocha.silveira@hotmail.com>
2021-02-23 20:15:05 -0300

    create migration, controller, model and route to users

---
+++ api/src/controllers/UserController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+
+import { User } from "../models/User";
+
+class UserController {
+  async create(req: Request, res: Response) {
+    const { name, email } = req.body;
+
+    const usersRepository = getRepository(User);
+
+    const userAlredyExists = await usersRepository.findOne({ email });
+
+    if (userAlredyExists) {
+      return res.status(400).json({
+        error: 'User alredy exists!'
+      })
+    }
+
+    const user = usersRepository.create({
+      name,
+      email
+    });
+
+    await usersRepository.save(user);
+
+    return res.status(201).json(user);
+  }
+};
+
+export { UserController };
--- api/src/server.ts
+++ api/src/server.ts
@@ -2,11 +2,11 @@ import 'reflect-metadata';
 import express from 'express';
 
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.post('/', (req, res) => {
-  res.json({ message: 'Dados salvos com sucesso' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'));
